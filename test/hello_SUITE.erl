5d604e3f78d70a65c3b4e7ca6db05d8824161b48
GalaxyGorilla <skattelmann@gmail.com>
2016-03-23 16:51:07 +0100

    second test working

--- test/hello_SUITE.erl
+++ test/hello_SUITE.erl
@@ -8,12 +8,11 @@
 % ---------------------------------------------------------------------
 % -- test cases
 bind_http(_Config) ->
-    [bind_url(?HTTP, Protocol) || Protocol <- ?PROTOCOLS],
-    timer:sleep(1000),
-    io:format(standard_error, "~p~n", [exometer_report:list_metrics([hello])]),
-    ok.
+    timer:sleep(100),
+    [bind_url(?HTTP, Protocol) || Protocol <- ?PROTOCOLS].
 
 bind_zmq_tcp(_Config) ->
+    timer:sleep(100),
     [bind_url(?ZMQ_TCP, Protocol) || Protocol <- ?PROTOCOLS].
 
 unbind_all(_Config) ->
@@ -76,8 +75,8 @@ keep_alive(_Config) ->
 % ---------------------------------------------------------------------
 % -- common_test callbacks
 all() ->
-    [bind_http
-    % bind_zmq_tcp,
+    [bind_http,
+     bind_zmq_tcp
     % unbind_all,
     % start_supervised,
     % start_named_supervised,
